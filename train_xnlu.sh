488b47657933c4784826f4407ae59a80c7e215f9
Henry Lucky <henrylucky68@gmail.com>
2021-10-08 15:47:44 +0700

    Update for Datacrunch.io 2

--- train_xnlu.sh
+++ train_xnlu.sh
@@ -5,6 +5,7 @@ python train.py '../data/data_dir_xNLU' \
   --lr-scheduler inverse_sqrt --warmup-updates 10000 --clip-norm 25.0 \
   --lr 5e-5 --warmup-init-lr 1e-7 --min-lr 1e-9 --device-id 0 --no-epoch-checkpoints --save-interval-updates 5000 \
   --criterion label_smoothed_length_cross_entropy --label-smoothing 0.1 \
-  --weight-decay 0.0 --max-tokens 4096 --update-freq 5 --max-update 300000 \
+  --weight-decay 0.001 --max-tokens 4096 --update-freq 5 --max-update 300000 \
   --left-pad-source False --max-source-positions 512 --max-target-positions 512 \
-  --bert-model-name indolem/indobert-base-uncased --decoder-bert-model-name indobenchmark/indobert-base-p2
+  --bert-model-name indolem/indobert-base-uncased --decoder-bert-model-name indobenchmark/indobert-base-p2 \
+  --save-dir checkpoints_xnlu
